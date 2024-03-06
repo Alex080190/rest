1ae3e85693aa38fe146dbb31d676d81b138c4e8d
Alex080190 <Alex080190@mail.ru>
2024-03-06 15:14:45 +0300

    Add jolokia-agent

--- src/main/java/com/example/rest/Beans/User.java
+++ src/main/java/com/example/rest/Beans/User.java
@@ -3,16 +3,29 @@ package com.example.rest.Beans;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Credentials {
+public class User {
     private String login;
 
     private String password;
 
     private String date;
 
-    public Credentials(String login, String password) {
+    private String email;
+
+    public User() {
+    }
+
+    public User(String login, String password, String email) {
         this.login = login;
         this.password = password;
+        this.email = email;
+    }
+
+    public User(String login, String password, String date, String email) {
+        this.login = login;
+        this.password = password;
+        this.date = date;
+        this.email = email;
     }
 
     public String getLogin() {
@@ -39,11 +52,20 @@ public class Credentials {
         this.date = date;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
-        return "Credentials{" +
+        return "User{" +
                 "login='" + login + '\'' +
                 ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
--- src/main/java/com/example/rest/Controller/ApiController.java
+++ src/main/java/com/example/rest/Controller/ApiController.java
@@ -1,10 +1,14 @@
 package com.example.rest.Controller;
 
-import com.example.rest.Beans.Credentials;
+import com.example.rest.Beans.User;
+import com.example.rest.DB.ClientDBFunctions;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -13,35 +17,45 @@ public class ApiController {
 
 
     @GetMapping("/get")
-    public String getPage() throws JsonProcessingException {
+    public String getPage(@RequestParam String login) throws JsonProcessingException, SQLException {
         ObjectMapper objectMapper = new ObjectMapper();
+        ClientDBFunctions userFromDB = new ClientDBFunctions();
 
-        Credentials credentials = new Credentials("TestLogin", "TestPassword");
-
-        return objectMapper.writeValueAsString(credentials);
+        User user = userFromDB.getUserByLogin(login);
+        System.out.println(user);
+        if (user == null) {
+            return "No such login in DB \n" + new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return objectMapper.writeValueAsString(user);
     }
     @PostMapping("/post")
-    public String getData(@RequestBody Credentials receivedCredentials) throws JsonProcessingException {
+    public String getData(@RequestBody User receivedUser) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
 
-        String login = receivedCredentials.getLogin();
-        String password = receivedCredentials.getPassword();
+        String login = receivedUser.getLogin();
+        String password = receivedUser.getPassword();
+        String email = receivedUser.getEmail();
 
-        if (login == null || password == null) {
-            throw new SecurityException("The entered data does not match the format, " +
+        if (login == null || password == null || email == null) {
+            return "The entered data does not match the format, " +
                     "the correct format - " +
                     "login : Your login, " +
-                    "password : Your password");
+                    "password : Your password " +
+                    "email: Your email\n" + new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        Credentials credentials = new Credentials(login, password);
+        User user = new User(login, password, email);
 
         Date current = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         String date = dateFormat.format(current);
 
-        credentials.setDate(date);
+        user.setDate(date);
 
-        return objectMapper.writeValueAsString(credentials);
+        ClientDBFunctions userToDB = new ClientDBFunctions();
+
+        userToDB.insertUser(user);
+
+        return objectMapper.writeValueAsString(user);
     }
 }
